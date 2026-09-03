0dfb66347993423f35288567b6a267daf3bf00c3
Shantanu Gawande <shantanudgawande@gmail.com>
2026-09-03 16:44:14 +0530

    Add files via upload

---
+++ FBS/Core Python/Demos/Patterns/p1.py
@@ -0,0 +1,19 @@
+print()
+for i in range (1,6):
+    for j in range(1,6):
+        print('*', end = ' ')
+    print()
+
+
+for i in range (1,6):
+    for j in range (1,6):
+        print( i , end=' ')
+    print()
+
+
+for i in range (1,6):
+    for j in range (1,6):
+        print( j , end=' ')
+    print()
+
+
