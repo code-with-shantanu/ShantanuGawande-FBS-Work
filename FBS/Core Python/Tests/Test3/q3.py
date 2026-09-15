b2c5c801b282e2efa61b8e290c52a87fcd22555c
Shantanu Gawande <shantanudgawande@gmail.com>
2026-09-15 14:40:17 +0530

    Add files via upload

---
+++ FBS/Core Python/Tests/Test3/q3.py
@@ -0,0 +1,21 @@
+n = int(input("Enter number of employees: "))
+total = 0
+
+for i in range(n):
+    basic = int(input("Enter basic salary: "))
+
+    if basic < 20000:
+        da = basic * 10 / 100
+        ta = basic * 12 / 100
+        hra = basic * 15 / 100
+    else:
+        da = basic * 15 / 100
+        ta = basic * 18 / 100
+        hra = basic * 20 / 100
+
+    salary = basic + da + ta + hra
+    print("Total salary =", salary)
+
+    total = total + salary
+
+print("Total salary of all employees =", total)
