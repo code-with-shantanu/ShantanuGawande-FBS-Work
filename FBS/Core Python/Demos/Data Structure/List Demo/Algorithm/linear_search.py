68d39306fea2b9b9b2625c8e5eaf952585300c20
Shantanu Gawande <shantanudgawande@gmail.com>
2026-09-19 12:45:04 +0530

    Add files via upload

---
+++ FBS/Core Python/Demos/Data Structure/List Demo/Algorithm/linear_search.py
@@ -0,0 +1,17 @@
+def linearSearch(li, search_ele):
+    size = len(li)
+    for ind in range(0, size):
+        if(li[ind] == search_ele):
+            return ind
+
+    else:
+        return-1
+
+li = [40, 50, 60, 20, 10]
+ele = 80
+res = linearSearch(li, ele)
+
+if(res != -1):
+    print(f'{ele} is present at index {res}.')
+else:
+    print(f'{ele} is not present in list.')
